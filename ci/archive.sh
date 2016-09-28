dcc902568b61830f669ec273c8e9c49268e3a802
David Bazile <dbazile@radiantblue.com>
2016-09-28 14:55:40 -0400

    CI: Workaround for scipy/numpy compilation failures on build agent

--- ci/archive.sh
+++ ci/archive.sh
@@ -10,9 +10,20 @@ source $root/ci/vars.sh
 
 pip_binary=$(which pip || which pip3)
 
+# Fetch libraries
 mkdir -p vendor
 $pip_binary install --download vendor -r requirements.txt
 
+# HACK HACK HACK HACK HACK HACK HACK HACK HACK
+# FIXME -- determine why Jenkins build agent is unable to retrieve the following files
+pushd vendor
+rm numpy* scipy*
+curl -O "https://pypi.python.org/packages/b3/46/3aecb4feaa2ef3c4071a9a853a35a2695f7677ebc7731c3cb3d291c6d188/scipy-0.18.0-cp27-cp27m-manylinux1_x86_64.whl"
+curl -O "https://pypi.python.org/packages/ae/34/1b5838ae482992fcc4f2d00ced6bb1dde58f4abb352a4af65a9f13ce9dd7/numpy-1.11.1-cp27-cp27m-manylinux1_x86_64.whl"
+popd
+# HACK HACK HACK HACK HACK HACK HACK HACK HACK
+
+# Extract historical tidal data
 tar -xzvf fdh.sqlite.tar.gz -C data
 
 

e2a619330bd8fae18906c3608c671beeae61631e
Patrick D <patrick.doody@rgi-corp.com>
2017-06-26 14:04:27 -0400

    Add python setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='bf-tideprediction',
+    version='0.1',
+    description='Tide prediction application',
+    long_description='This module predicts tides based on time and location information.',
+    url='https://github.com/venicegeo/bf-tideprediction',
+    author='Venice Geo',
+    license='Apache 2',
+    packages=['bftideprediction']
+)

70f630fc2a14d919a569c2c892b8ca3b21de7a8d
Patrick D <patrick.doody@rgi-corp.com>
2017-06-26 08:57:37 -0400

    Add Conda Build

---
+++ docker/conda-build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "Starting conda build..."
+export PATH="$HOME/miniconda2/bin:$PATH"
+pushd `dirname $0`/tide-repo > /dev/null
+root=$(pwd -P)
+echo "***Updating conda..***"
+conda update -n root conda-build -y
+conda update --all -y
+popd > /dev/null
+cp -r $HOME/miniconda2/conda-bld /$HOME/conda-repo
